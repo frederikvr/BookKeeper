364e9ab76fc8f25cc40c653ca5d56592b5c3d6c6
FrederikVr <frederik.vanruyskensvelde@gmail.com>
2019-12-21 14:41:09 +0100

    Initial Commit

---
+++ BookKeeper2Tests/BookKeeper2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  BookKeeper2Tests.swift
+//  BookKeeper2Tests
+//
+//  Created by Fred on 21/12/2019.
+//  Copyright © 2019 Fred. All rights reserved.
+//
+
+import XCTest
+@testable import BookKeeper2
+
+class BookKeeper2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
