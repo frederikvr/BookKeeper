c1435bffd1e77dbeab44c9c31b8fc54be0341121
FrederikVr <frederik.vanruyskensvelde@gmail.com>
2019-12-22 14:59:05 +0100

    initial commit

--- BookKeeper2/ViewController.swift
+++ BookKeeper2/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITextFieldDelegate {
+    //MARK: Properties
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var bookNameLabel: UILabel!
+    
+    @IBOutlet weak var testTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Handle the text field's user input through delegate callbacks
+        // "self" refers to ViewController class, this works because we adopted the UITextFieldDelegate protocol in class declaration
+        nameTextField.delegate = self
     }
-
-
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // this method is called when the user presses Return
+        
+        // Hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        // this method is called after the textfield resigns it's FirstResponder status (FirstResponder means it's the main receiving object that moment)
+        bookNameLabel.text = textField.text
+    }
+    //MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        bookNameLabel.text = "Default Text"
+    }
 }
 
